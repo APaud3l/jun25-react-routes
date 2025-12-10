bc8b233265e20efa82e387f340c3e302ba2f7f36
APaud3l <aamod.paudel@coderacademy.edu.au>
2025-12-10 19:35:17 +1100

    feat: Initial setup, Defined HomePage and AboutPage and referenced them using Routes

---
+++ src/pages/AboutPage.jsx
@@ -0,0 +1,11 @@
+
+function AboutPage(){
+    return(
+        <div>
+            <h1>About</h1>
+            <p>The app is just here to help us learn about routing.</p>
+        </div>
+    )
+}
+
+export default AboutPage;
---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,11 @@
+
+function HomePage(){
+    return(
+        <div>
+            <h1>Home</h1>
+            <p>Welcome to our REACT ROUTER DEMO SPA.</p>
+        </div>
+    )
+}
+
+export default HomePage;

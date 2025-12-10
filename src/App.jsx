0e9323b41cf1340375a638e6ba3fdd6671cb9509
APaud3l <aamod.paudel@coderacademy.edu.au>
2025-12-10 19:45:34 +1100

    feat: Defined NavLinks to link to Home and About from the UI

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,41 @@
-import { Routes, Route } from 'react-router-dom';
-import HomePage from './pages/HomePage';
-import AboutPage from './pages/AboutPage';
+import { Routes, Route, Link, NavLink } from "react-router-dom";
+import HomePage from "./pages/HomePage";
+import AboutPage from "./pages/AboutPage";
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={ <HomePage /> } />
-      <Route path="/about" element={ <AboutPage /> } />
-    </Routes>
-  )
+    <>
+      <nav>
+        {/* Navigation using Link  */}
+        {/* <Link to="/">Home</Link>
+      <Link to="/about">About</Link> */}
+
+        {/* Navitaion using NavLink */}
+        <NavLink
+          to="/"
+          style={({ isActive }) => ({
+            marginRight: "1rem",
+            fontWeight: isActive ? "bold" : "normal",
+          })}
+        >
+          Home
+        </NavLink>
+        <NavLink
+          to="/about"
+          style={({ isActive }) => ({
+            marginRight: "1rem",
+            fontWeight: isActive ? "bold" : "normal",
+          })}
+        >
+          About
+        </NavLink>
+      </nav>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/about" element={<AboutPage />} />
+      </Routes>
+    </>
+  );
 }
 
-export default App
+export default App;
